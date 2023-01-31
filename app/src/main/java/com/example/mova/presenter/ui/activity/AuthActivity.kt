3aa0f3e569d478a59b2137db12906d615f3d80cb
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2023-01-31 15:49:51 +0400

    initial commit

---
+++ app/src/main/java/com/example/mova/presenter/ui/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mova.presenter.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mova.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
--- app/src/main/java/com/example/mova/presenter/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/mova/presenter/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mova
+package com.example.mova.presenter.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mova.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
